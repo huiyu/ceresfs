76c4f6dc18a4e8d6d744a64766f8666416341b62
余晖 <huiyu219@gmail.com>
2017-01-03 10:43:15 +0800

    add decompose methods

--- ceresfs-server/src/main/java/com/supconit/ceresfs/util/NumericUtil.java
+++ ceresfs-server/src/main/java/com/supconit/ceresfs/util/NumericUtil.java
@@ -6,7 +6,23 @@ public class NumericUtil {
         return ((int) x) << 16 | y & 0xffff;
     }
 
+    public static int highOfLong(long x) {
+        return (int) (x >> 32);
+    }
+
+    public static int lowOfLong(long x) {
+        return ((int) x);
+    }
+
     public static long combineTwoInts(int x, int y) {
         return (((long) x) << 32) | (y & 0xffffffffL);
+    }
+    
+    public static short highOfInt(int x) {
+        return ((short) (x >> 16));
+    }
+    
+    public static short lowOfInt(int x) {
+        return ((short) x);
     }
 }
