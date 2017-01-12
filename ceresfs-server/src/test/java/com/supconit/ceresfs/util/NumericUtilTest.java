3eaeb63a272bdd5b4555f7c38afbf79c9c58bacf
余晖 <huiyu219@gmail.com>
2017-01-12 14:57:53 +0800

    Add NumericUtilTest

--- ceresfs-server/src/test/java/com/supconit/ceresfs/util/NumericUtilTest.java
+++ ceresfs-server/src/test/java/com/supconit/ceresfs/util/NumericUtilTest.java
@@ -34,4 +34,32 @@ public class NumericUtilTest {
             assertEquals(expected, NumericUtil.combineTwoInts(x, y));
         }
     }
+
+    @Test
+    public void testSplitInt() throws Exception {
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int x = random.nextInt();
+            ByteBuffer buffer = ByteBuffer.allocate(4);
+            buffer.putInt(x).flip();
+            short y = buffer.getShort();
+            short z = buffer.getShort();
+            assertEquals(y, NumericUtil.highOfInt(x));
+            assertEquals(z, NumericUtil.lowOfInt(x));
+        }
+    }
+
+    @Test
+    public void testSplitLong() throws Exception {
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            long x = random.nextLong();
+            ByteBuffer buffer = ByteBuffer.allocate(8);
+            buffer.putLong(x).flip();
+            int y = buffer.getInt();
+            int z = buffer.getInt();
+            assertEquals(y, NumericUtil.highOfLong(x));
+            assertEquals(z, NumericUtil.lowOfLong(x));
+        }
+    }
 }
